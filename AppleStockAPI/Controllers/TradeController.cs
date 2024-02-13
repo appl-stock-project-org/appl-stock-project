708aaeac3e44b8bdd5e9d11bf3c7f77431450620
PaavoNykanen <paavonykanen@gmail.com>
2024-02-13 20:58:06 +0200

    feat: Added recording trades
    - Added new trade controller for keeping track of trades and adding, clearing and getting them
    - Added unit tests for trade controller

---
+++ AppleStockAPI/Controllers/TradeController.cs
@@ -0,0 +1,52 @@
+﻿using AppleStockAPI.Models;
+using System;
+using static System.DateTime;
+
+namespace AppleStockAPI.Controllers
+{
+    /// <summary>
+    /// Class to handle trades
+    /// </summary>
+    public class TradeController
+    {
+        private List<Trade> trades;
+
+        public TradeController() 
+        {
+            trades = new List<Trade>();
+        }
+
+        /// <summary>
+        /// Function to save a record of a trade. Price and quantity must be positive
+        /// </summary>
+        /// <param name="price">Price of the trade</param>
+        /// <param name="quantity">Quatity of traded stock</param>
+        public void RecordTrade(double price, int quantity) 
+        {
+            if (price <= 0 || quantity <= 0) return;
+            var trade = new Trade()
+            { 
+                Price = price,
+                Quantity = quantity,
+            };
+            trades.Add(trade);
+        }
+
+        /// <summary>
+        /// Function to fetch all recorded trades
+        /// </summary>
+        /// <returns>List of recorded trades</returns>
+        public List<Trade> GetTrades() 
+        {  
+            return trades;
+        }
+
+        /// <summary>
+        /// Function to empty the trades list
+        /// </summary>
+        public void ClearTrades()
+        {
+            trades.Clear();
+        }
+    }
+}
---
+++ AppleStockAPI/Models/Trade.cs
@@ -0,0 +1,14 @@
+﻿namespace AppleStockAPI.Models
+{
+    /// <summary>
+    /// Class for Trade objects
+    /// </summary>
+    public class Trade
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+        public DateTime TradeTime { get; set; } = DateTime.Now;
+    }
+}
