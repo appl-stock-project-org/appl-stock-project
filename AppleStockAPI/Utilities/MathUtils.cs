45796d58e83e2ae079bb02529f56e99209f5cac4
tomb-cyber <bjorn.tommi@gmail.com>
2024-03-11 12:09:17 +0200

    comment change

--- AppleStockAPI/Utilities/MathUtils.cs
+++ AppleStockAPI/Utilities/MathUtils.cs
@@ -1,7 +1,11 @@
 namespace AppleStockAPI.Utilities {
     public static class MathUtils
     {
-        // Example utility method 1
+        /// <summary>
+        /// Round a double to two decimals by truncating, so e.g. 10.9999 -> 10.99
+        /// </summary>
+        /// <param name="num">Number to truncate</param>
+        /// <returns>Truncated number upto 2 decimals</returns>
         public static double TruncateTo2Decimals(double num)
         {
             return Math.Truncate(num * 100) / 100;
